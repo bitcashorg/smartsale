b38d43a9e837a8e461e18eed8247137f16741133
Gabo Esquivel <contact@gaboesquivel.com>
2024-01-21 16:57:57 -0600

    chore(webapp): remove unused hooks

--- apps/webapp/app/page.tsx
+++ apps/webapp/app/page.tsx
@@ -1,5 +1,10 @@
 import { Newsletter } from "@/components/newletter";
 import {Upcoming} from "@/components/upcoming";
+import _ from "lodash";
+import { TestnetEasyAuction } from "smartevm-abis";
+
+console.log(_.groupBy(TestnetEasyAuction.abi.map(o => _.omit(o, ['inputs','outputs'])), 'type'))
+
 
 export default async function IndexPage() {
   return <div className="container">
