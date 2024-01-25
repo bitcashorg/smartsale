ce9115aac07fea0e80408631508c0023db83cd5a
Gabo Esquivel <contact@gaboesquivel.com>
2024-01-24 18:39:35 -0600

    chore(webapp): get MBOTSCred auction data

--- apps/webapp/components/auction-debug.tsx
+++ apps/webapp/components/auction-debug.tsx
@@ -1,8 +1,12 @@
 'use client'
 
-import { useAuctionData } from "@/hooks/use-auction-data"
+import { useAuctionData } from '@/hooks/use-auction-data'
 
 export function AuctionDebug() {
-  const auction = useAuctionData(1)
-  return <pre><code>{JSON.stringify(auction,null,2)}</code></pre>
+  const auction = useAuctionData(3) //MBOTCred
+  return (
+    <pre>
+      <code>{JSON.stringify(auction, null, 2)}</code>
+    </pre>
+  )
 }
