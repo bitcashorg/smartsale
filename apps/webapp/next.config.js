70f7ff559ada496ad0ccbc00757b5c32e3ee9705
Gabo Esquivel <contact@gaboesquivel.com>
2024-02-21 10:34:26 -0600

    wip(webapp): add @prisma/nextjs-monorepo-workaround-plugin

--- apps/webapp/next.config.js
+++ apps/webapp/next.config.js
@@ -2,7 +2,16 @@
 
 const path = require('path')
 
+const { PrismaPlugin } = require('@prisma/nextjs-monorepo-workaround-plugin')
+
 module.exports = {
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()]
+    }
+
+    return config
+  },
   images: {
     remotePatterns: [
       {
