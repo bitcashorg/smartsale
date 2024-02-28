c445647b17835dc803ac19ffe98ecec2ef10d126
Gabo Esquivel <contact@gaboesquivel.com>
2024-02-28 16:14:05 -0600

    wip: wallet page

--- apps/webapp/app/wallet/page.tsx
+++ apps/webapp/app/wallet/page.tsx
@@ -55,12 +55,12 @@ function WalletBalances() {
           </TableRow>
         ))}
       </TableBody>
-      <TableFooter>
+      {/* <TableFooter>
         <TableRow>
           <TableCell colSpan={3}>Total</TableCell>
           <TableCell className="text-right">$2,500.00</TableCell>
         </TableRow>
-      </TableFooter>
+      </TableFooter> */}
     </Table>
   )
 }
