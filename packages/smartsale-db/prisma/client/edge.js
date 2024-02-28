17623e8ad30aee6acad55469f9b53bb2a529477b
Gabo Esquivel <contact@gaboesquivel.com>
2024-02-28 13:30:39 -0600

    debug: update prisma client

--- packages/smartsale-db/prisma/client/edge.js
+++ packages/smartsale-db/prisma/client/edge.js
@@ -194,7 +194,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": null,
+    "rootEnvPath": "../../.env",
     "schemaEnvPath": "../../.env"
   },
   "relativePath": "..",
