6d4560f7434fef2eb1028bc02aed3e3655c1d989
Gabo Esquivel <contact@gaboesquivel.com>
2024-02-29 10:03:10 -0600

    chore: update prisma client

--- packages/smartsale-db/prisma/client/index.js
+++ packages/smartsale-db/prisma/client/index.js
@@ -195,7 +195,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": null,
+    "rootEnvPath": "../../.env",
     "schemaEnvPath": "../../.env"
   },
   "relativePath": "..",
