31eee5a68270014f78e0621f932eeeaa63eea4d8
Gabo Esquivel <contact@gaboesquivel.com>
2024-03-20 08:29:09 -0600

    chore: try catch on main

--- apps/indexer/src/index.ts
+++ apps/indexer/src/index.ts
@@ -1,11 +1,15 @@
+import { getErrorMessage } from 'smartsale-lib'
 import { startAuctionIndexer } from './modules/auction'
 import { startSwapsService } from './modules/swaps'
 
 async function main() {
-  startAuctionIndexer()
-  startSwapsService()
-
   console.log(`Indexer running ...`)
+  try {
+    startAuctionIndexer()
+    startSwapsService()
+  } catch (error) {
+    console.log('ERROR:' + getErrorMessage(error), JSON.stringify(error))
+  }
 }
 
 main()
