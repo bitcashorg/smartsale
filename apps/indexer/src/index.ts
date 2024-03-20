6a6024fdc920cdf1ef4debb64458f8add691c41e
Gabo Esquivel <contact@gaboesquivel.com>
2024-03-20 08:26:46 -0600

    chore: remove express

--- apps/indexer/src/index.ts
+++ apps/indexer/src/index.ts
@@ -1,9 +1,3 @@
-import express from 'express'
-
-const app = express()
-const port = 8080
-
-// Assuming `startIndexer` initializes Viem and sets it up for use
 import { startAuctionIndexer } from './modules/auction'
 import { startSwapsService } from './modules/swaps'
 
@@ -11,9 +5,7 @@ async function main() {
   startAuctionIndexer()
   startSwapsService()
 
-  app.listen(port, () => {
-    console.log(`Indexer running ...`)
-  })
+  console.log(`Indexer running ...`)
 }
 
 main()
