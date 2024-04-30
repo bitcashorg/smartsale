d60a5248136f73866a7bbfdb0fa6954f8ff2ede4
Nathanael Liu <supernathanliu@gmail.com>
2024-04-30 18:42:01 +0000

    fix: the horizontal alignment of nav links on header

--- apps/webapp/components/layout/header.tsx
+++ apps/webapp/components/layout/header.tsx
@@ -12,11 +12,11 @@ export function Header() {
   return (
     <div className="sticky top-0 z-50 flex h-16 bg-background md:p-4 lg:p-10">
       <div className="container flex items-center justify-between bg-background">
-        <div className="flex h-full items-center">
+        <div className="flex h-full items-center grow">
           <Link shallow href="/">
             <IconBitlauncher />
           </Link>
-          <div className="hidden md:flex md:gap-3 md:pl-4 lg:gap-10 lg:pl-16">
+          <div className="hidden md:flex md:gap-3 md:pl-4 lg:gap-10 lg:pl-16 justify-center grow">
             <NavLinks />
           </div>
         </div>
