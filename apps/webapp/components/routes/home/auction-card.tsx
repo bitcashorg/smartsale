574fc35d9530ae126e079136cdc4c04a2b1b4a35
Gabo Esquivel <contact@gaboesquivel.com>
2024-05-13 12:51:20 -0600

    fix: img load mobile nav (#195)
    * chore: restore wallet connect provider
    
    * fix: image loading and mobile nav padding
    
    * chore: update bun.lock

--- apps/webapp/components/routes/home/auction-card.tsx
+++ apps/webapp/components/routes/home/auction-card.tsx
@@ -35,7 +35,6 @@ export function AuctionCard(project: Project) {
           placeholder="blur"
           className="h-[216px] w-full rounded-t-xl object-cover group-hover:shadow-xl"
           alt={title}
-          priority
           loading="lazy"
           sizes="(max-width: 320px) 280px, (max-width: 480px) 440px, 800px"
         />
@@ -45,15 +44,15 @@ export function AuctionCard(project: Project) {
       </figure>
       <div className="px-4 py-6 lg:px-9 lg:py-8">
         <h3 className="text-xl font-bold">{title}</h3>
-        <p className="mt-2 max-w-sm text-sm">{pitch}</p>
+        <p className="max-w-sm mt-2 text-sm">{pitch}</p>
       </div>
-      <div className="flex w-full flex-col items-center justify-between px-4 pb-6 md:mt-auto lg:pb-8 xl:px-9">
-        <ul className="mb-6 flex w-full flex-col gap-2 lg:mb-8">
-          <li className="flex w-full justify-between rounded-full bg-muted px-4 py-2">
+      <div className="flex flex-col items-center justify-between w-full px-4 pb-6 md:mt-auto lg:pb-8 xl:px-9">
+        <ul className="flex flex-col w-full gap-2 mb-6 lg:mb-8">
+          <li className="flex justify-between w-full px-4 py-2 rounded-full bg-muted">
             <span className="opacity-70">Fundraising Goal</span>
             <b>{fundraiseGoal}</b>
           </li>
-          <li className="flex w-full justify-between rounded-full bg-muted px-4 py-2">
+          <li className="flex justify-between w-full px-4 py-2 rounded-full bg-muted">
             <span className="opacity-70">Max Allocation</span>
             <b>{maxAllocation}</b>
           </li>
