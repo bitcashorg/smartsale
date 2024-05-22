bb4b4beee788370776ea327c0c390e216ba8588c
TopETH <supernathanliu@gmail.com>
2024-05-21 20:07:02 -0400

    feat: add time allow origin to header

--- apps/webapp/next.config.js
+++ apps/webapp/next.config.js
@@ -20,6 +20,15 @@ const nextConfig = {
               'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
           }
         ]
+      },
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Timing-Allow-Origin',
+            value: '*' // Adjust the origin as needed
+          }
+        ]
       }
     ]
   },
