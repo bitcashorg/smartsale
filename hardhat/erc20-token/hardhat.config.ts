173dc484828e94113fe5889fb4dac6280f0c56a6
Leandro Gavidia Santamaria <93232139+leandrogavidia@users.noreply.github.com>
2024-05-27 10:35:25 -0400

    chore: lgs setup arbitrum networks (#1)
    * config: add arbitrum one and arbitrum sepolia networks on the hardhat config file
    
    * chore: remove eos evm networks

--- hardhat/erc20-token/hardhat.config.ts
+++ hardhat/erc20-token/hardhat.config.ts
@@ -4,20 +4,20 @@ require("dotenv").config();
 module.exports = {
   solidity: "0.7.6",
   networks: {
-    eosevm_testnet: {
-      accounts: [process.env.PK],
-      url: "https://api.testnet.evm.eosnetwork.com",
-      chainId: 15557,
-    },
-    eosevm_mainnet: {
-      accounts: [process.env.PK],
-      url: "https://api.evm.eosnetwork.com",
-      chainId: 17777,
-    },
     sepolia: {
       accounts: [process.env.PK],
       url: "https://rpc.sepolia.org",
       chainId: 11155111,
+    },
+    arbitrumOne: {
+      accounts: [process.env.PK],
+      url: 'https://arb1.arbitrum.io/rpc',
+      chainId: 42161
+    },
+    arbitrumSepolia: {
+      accounts: [process.env.PK],
+      url: 'https://sepolia-rollup.arbitrum.io/rpc',
+      chainId: 421614
     },
   },
 };
