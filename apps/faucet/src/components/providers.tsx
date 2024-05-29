fb927b2a3ee704ce3dafd45a92fa37802a78e246
Gabo Esquivel <contact@gaboesquivel.com>
2024-05-29 09:34:11 -0600

    chore(faucet): update provider config

--- apps/faucet/src/components/providers.tsx
+++ apps/faucet/src/components/providers.tsx
@@ -2,6 +2,11 @@ import { ReactNode } from "react";
 
 import { WagmiProvider } from "wagmi";
 import { eosEvmTestnet } from "smartsale-env";
+import {
+  metaMaskWallet,
+  trustWallet,
+  walletConnectWallet
+} from '@rainbow-me/rainbowkit/wallets'
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit";
@@ -12,6 +17,12 @@ const queryClient = new QueryClient();
 export const wagmiConfig = getDefaultConfig({
   appName: "Bitlauncher",
   projectId: "YOUR_PROJECT_ID",
+   wallets: [
+    {
+      groupName: 'Popular',
+      wallets: [metaMaskWallet, trustWallet, walletConnectWallet]
+    }
+  ],
   chains: [{ ...eosEvmTestnet, fees: undefined }, sepolia],
 });
 
