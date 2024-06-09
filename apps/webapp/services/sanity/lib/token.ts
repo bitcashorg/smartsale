48e8e9ae81d4c3c02c91467dbd05fbbc006c9ac9
Gabo Esquivel <contact@gaboesquivel.com>
2024-06-09 14:54:01 -0600

    chore: debug read token issue

--- apps/webapp/services/sanity/lib/token.ts
+++ apps/webapp/services/sanity/lib/token.ts
@@ -2,7 +2,10 @@ import 'server-only'
 
 import { experimental_taintUniqueValue } from 'react'
 
-export const token = process.env.SANITY_API_READ_TOKEN
+// TODO: solve this process env var issue and use a new token
+export const token =
+  process.env.SANITY_API_READ_TOKEN ||
+  'sk7ZTFH9wFGkk3g82SgLehGuphXDRtXVGih2dXhRIOQQSLbUbyE01Ww7m3tAD1zTwGFA3daV2uA0vk8HnEjRj1tKcbJjazSGkqMECdYcItgmnauwFteFYIXqFWNb8YH404xfKZNIcP8KpXRnLt2W1QVYPBYnHVfbKWF2LeyB7JOseuv76Zj8'
 
 console.log('token.ts SANITY_API_READ_TOKEN', process.env.SANITY_API_READ_TOKEN)
 
