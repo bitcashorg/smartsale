5c185e344ad9aaa2c8e22d8f1fc042681f6ff06b
Gabo Esquivel <contact@gaboesquivel.com>
2024-06-10 12:05:15 -0600

    chore: update bun lock

--- apps/webapp/services/sanity/lib/token.ts
+++ apps/webapp/services/sanity/lib/token.ts
@@ -7,7 +7,7 @@ export const token =
   process.env.SANITY_API_READ_TOKEN ||
   'sk7ZTFH9wFGkk3g82SgLehGuphXDRtXVGih2dXhRIOQQSLbUbyE01Ww7m3tAD1zTwGFA3daV2uA0vk8HnEjRj1tKcbJjazSGkqMECdYcItgmnauwFteFYIXqFWNb8YH404xfKZNIcP8KpXRnLt2W1QVYPBYnHVfbKWF2LeyB7JOseuv76Zj8'
 
-console.log('token.ts SANITY_API_READ_TOKEN', process.env.SANITY_API_READ_TOKEN)
+// console.log('token.ts SANITY_API_READ_TOKEN', process.env.SANITY_API_READ_TOKEN)
 
 if (!token) {
   throw new Error('Missing SANITY_API_READ_TOKEN')
