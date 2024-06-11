dd3b01e4880fcc1c4cd92c37e3d2bbac348e6d81
Gabo Esquivel <contact@gaboesquivel.com>
2024-06-11 11:49:02 -0600

    wip: recent articles component

--- apps/webapp/components/layout/footer/footer.tsx
+++ apps/webapp/components/layout/footer/footer.tsx
@@ -2,6 +2,7 @@ import { LangProp } from '@/types/routing.type'
 import Participate from '@/components/layout/footer/participate'
 import dynamic from 'next/dynamic'
 import { FAQ } from './faq'
+import { RecentArticles } from './recents-articles'
 
 const DynamicNewsletter = dynamic(() => import('./newsletter') as any, {
   ssr: false
@@ -11,6 +12,7 @@ export function Footer({ lang }: LangProp) {
   return (
     <footer className="container flex flex-col flex-1 w-full gap-32 px-4 py-16 overflow-hidden">
       <Participate lang={lang} />
+      <RecentArticles lang={lang} />
       <FAQ lang={lang} />
       <DynamicNewsletter />
     </footer>
--- apps/webapp/components/routes/blog/sections.tsx
+++ apps/webapp/components/routes/blog/sections.tsx
@@ -4,15 +4,17 @@ import { ArticlesSection } from '@/services/datocms'
 import { ArticleCard } from './article-card'
 import { LucideIcons } from './lucide-icons'
 import { LangProp } from '@/types/routing.type'
+const fs = require('fs')
 
 export function BlogSections({ sections, lang }: BlogSectionsProps) {
+  fs.writeFileSync('sectionData.json', JSON.stringify(sections[0]))
   return (
-    <div className="flex w-full flex-col items-center justify-start py-10">
+    <div className="flex flex-col items-center justify-start w-full py-10">
       {sections.map(
         section =>
           section?.articles?.length > 0 && (
             <section className="container mb-10" key={section.name}>
-              <div className="mb-space-32 flex items-center justify-between text-xl">
+              <div className="flex items-center justify-between text-xl mb-space-32">
                 <span className="pl-3 font-bold text-black dark:text-white">
                   / {section.name}
                 </span>
@@ -27,7 +29,7 @@ export function BlogSections({ sections, lang }: BlogSectionsProps) {
                 </Link>
               </div>
 
-              <ul className="grid-cols-auto-dense grid w-full grid-cols-[repeat(auto-fill,minmax(300px,1fr))] flex-col gap-20 py-5 sm:flex-wrap md:gap-5 ">
+              <ul className="grid-cols-auto-dense grid w-full grid-cols-[repeat(auto-fill,minmax(300px,1fr))] flex-col gap-20 py-5 sm:flex-wrap md:gap-5">
                 {section?.articles?.map((post, index) => (
                   <ArticleCard
                     post={post}
