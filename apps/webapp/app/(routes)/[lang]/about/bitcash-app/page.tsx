2998bbdbc8afc2c36fe2adfcbe2b8739c5f3be0f
Gabo Esquivel <contact@gaboesquivel.com>
2024-06-14 11:37:17 -0600

    wip: navigation and site structure

--- apps/webapp/app/(routes)/[lang]/about/bitcash-app/page.tsx
+++ apps/webapp/app/(routes)/[lang]/about/bitcash-app/page.tsx
@@ -2,14 +2,78 @@ import React from 'react'
 import { Metadata } from 'next'
 import { CommonPageProps } from '@/types/routing.type'
 import { getDictionary } from '@/dictionaries'
+import {
+  CurrencyIcon,
+  MapIcon,
+  PhoneIcon,
+  QrCodeIcon,
+  ShieldIcon
+} from 'lucide-react'
+import {
+  SolutionLandingPageContent,
+  SolutionLandingPage
+} from '@/components/shared/solution-landing-page'
 
 export default async function BitcashAppPage({ params }: CommonPageProps) {
   const dict = await getDictionary(params.lang)
   return (
-    <div className="content-container !py-10 px-7 md:px-3 md:py-24">
-      <h1 className="heading">Bitcash App Page</h1>
+    <div className="!py-10 px-7 md:px-3 md:py-24">
+      <SolutionLandingPage content={content} />
     </div>
   )
+}
+
+const content: SolutionLandingPageContent = {
+  mainHeading: 'Buy Crypto, Pay Crypto.',
+  subHeading: 'The future of payments is here',
+  description:
+    'Seamlessly buy and pay with cryptocurrencies using our secure and user-friendly platform.',
+  features: [
+    {
+      title: 'Buy Crypto on the go',
+      description:
+        'Our mobile app allows you to buy and sell cryptocurrencies anytime, anywhere.',
+      icon: PhoneIcon
+    },
+    {
+      title: 'Pay with Crypto',
+      description:
+        'Use our QR code payment system to pay for goods and services with your cryptocurrencies.',
+      icon: QrCodeIcon
+    }
+  ],
+  business: {
+    heading: 'Trusted by businesses worldwide',
+    description:
+      'Join the growing number of businesses that accept and use cryptocurrencies for payments.'
+  },
+  advantages: [
+    {
+      title: 'Global Reach',
+      description:
+        'Our platform is available in over 180 countries, allowing you to transact globally.',
+      icon: MapIcon
+    },
+    {
+      title: 'Secure Transactions',
+      description:
+        'We use the latest security protocols to ensure your transactions are safe and secure.',
+      icon: ShieldIcon
+    },
+    {
+      title: 'Multiple Cryptocurrencies',
+      description:
+        'Our platform supports a wide range of cryptocurrencies, including Bitcoin, Ethereum, and more.',
+      icon: CurrencyIcon
+    }
+  ],
+  callToAction: {
+    heading: 'Get started today',
+    description:
+      'Join the future of payments and start accepting cryptocurrencies for your business.',
+    buttonLabel: 'Get Started',
+    buttonHref: '#'
+  }
 }
 
 export const metadata: Metadata = {
--- apps/webapp/app/(routes)/[lang]/about/investors/page.tsx
+++ apps/webapp/app/(routes)/[lang]/about/investors/page.tsx
@@ -2,14 +2,51 @@ import React from 'react'
 import { Metadata } from 'next'
 import { CommonPageProps } from '@/types/routing.type'
 import { getDictionary } from '@/dictionaries'
+import {
+  LandingPage,
+  LandingPageContent
+} from '@/components/shared/landing-page'
 
 export default async function InvestorsPage({ params }: CommonPageProps) {
   const dict = await getDictionary(params.lang)
   return (
-    <div className="content-container !py-10 px-7 md:px-3 md:py-24">
-      <h1 className="heading">Investors Page</h1>
+    <div className="!py-10 px-7 md:px-3 md:py-24">
+      <LandingPage content={content} />
     </div>
   )
+}
+
+const content: LandingPageContent = {
+  title: 'Robust Vetting Process',
+  header: 'Invest with Confidence',
+  description:
+    'Our rigorous vetting process ensures only the most promising web3 and AI projects make it onto the Bitlauncher platform. This gives investors peace of mind and helps them capitalize on the best opportunities.',
+  sections: [
+    {
+      subHeader: 'Thorough Audits',
+      text: "We conduct comprehensive technical, legal, and financial audits to validate each project's viability."
+    },
+    {
+      subHeader: 'Experienced Team',
+      text: 'Our team of industry experts carefully evaluates each project to ensure it meets our high standards.'
+    },
+    {
+      subHeader: 'Investor Protection',
+      text: 'Rigorous vetting protects investors from scams and low-quality projects, giving them confidence in their investments.'
+    },
+    {
+      subHeader: 'Curated Opportunities',
+      text: 'Our platform showcases only the most promising web3 and AI projects, saving investors time and effort.'
+    },
+    {
+      subHeader: 'Transparent Reporting',
+      text: 'We provide detailed and transparent reporting on all projects, ensuring investors have all the information they need.'
+    },
+    {
+      subHeader: 'Community Feedback Integration',
+      text: 'We incorporate feedback from our investor community to continuously improve the projects and the platform.'
+    }
+  ]
 }
 
 export const metadata: Metadata = {
