f9c738c6b905f54b2343674e87ad020e81c2ef70
Gabo Esquivel <contact@gaboesquivel.com>
2024-06-17 09:27:53 -0600

    wip: navigation and site structure

--- apps/webapp/components/layout/footer/footer.tsx
+++ apps/webapp/components/layout/footer/footer.tsx
@@ -1,11 +1,9 @@
-import { RecentArticles } from './recent-articles'
+import { RecentArticles } from './recent'
 import Participate from './participate'
 import { FAQ } from './faq'
 import { SiteLocale } from '@/services/datocms/graphql/generated/cms'
 import dynamic from 'next/dynamic'
 import { getDictionary } from '@/dictionaries'
-import { RecentAI } from './recent-ai'
-import { RecentMedia } from './recent-media'
 
 const DynamicNewsletter = dynamic(() => import('./newsletter') as any, {
   ssr: false
@@ -21,9 +19,7 @@ export default async function Footer({
     <footer className="container flex flex-col flex-1 w-full gap-32 px-4 py-16 overflow-hidden">
       <Participate lang={params.lang} dict={dict} />
       <FAQ lang={params.lang} />
-      <RecentAI lang={params.lang} />
       <RecentArticles lang={params.lang} />
-      <RecentMedia lang={params.lang} />
       <DynamicNewsletter />
     </footer>
   )
