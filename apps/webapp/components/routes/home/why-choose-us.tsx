7c6eb000ca19a10bf746d1e36e43ffad582c7a6c
Gabo Esquivel <contact@gaboesquivel.com>
2024-06-17 23:19:07 -0600

    chore: use correct parragraph class

--- apps/webapp/components/routes/home/why-choose-us.tsx
+++ apps/webapp/components/routes/home/why-choose-us.tsx
@@ -18,7 +18,7 @@ export function WhyChooseUs() {
               <Card className="flex flex-col items-center justify-center p-6 space-y-4">
                 <IconComponent className="w-12 h-12 text-gray-900 dark:text-gray-50" />
                 <h3 className="text-lg font-semibold">{feature.title}</h3>
-                <p className="text-sm text-center text-gray-500 dark:text-gray-400">
+                <p className="text-center text-gray-500 parragraph dark:text-gray-400">
                   {feature.description}
                 </p>
               </Card>
