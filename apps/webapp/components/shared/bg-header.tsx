f897f5e37f2be96baed66502677a323631480296
Gabo Esquivel <contact@gaboesquivel.com>
2024-07-08 17:44:36 -0600

    chore: bigger headings on mobile

--- apps/webapp/components/shared/bg-header.tsx
+++ apps/webapp/components/shared/bg-header.tsx
@@ -11,16 +11,16 @@ export function BgHeader({
   imageSrc: string
 }) {
   return (
-    <header className="relative top-0 z-0 flex min-h-96 flex-col items-center justify-center bg-cover bg-center py-10 md:py-24">
+    <header className="relative top-0 z-0 flex flex-col items-center justify-center py-10 bg-center bg-cover min-h-96 md:py-24">
       <Image
-        className="pointer-events-none absolute inset-0 bg-black bg-cover bg-center object-cover opacity-50"
+        className="absolute inset-0 object-cover bg-black bg-center bg-cover opacity-50 pointer-events-none"
         src={imageSrc}
         fill
         alt={heading}
         priority
       />
 
-      <h1 className="heading mb-6 flex max-w-[70%] justify-center text-center leading-loose drop-shadow-md md:text-6xl">
+      <h1 className="heading mb-6 flex max-w-[70%] justify-center text-center text-5xl leading-loose drop-shadow-md md:text-6xl">
         <Balancer>{heading}</Balancer>
       </h1>
 
