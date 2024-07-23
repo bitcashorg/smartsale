b7bef3ee93d399869fe18d6cf91fc929ba2f99b7
Gabo Esquivel <contact@gaboesquivel.com>
2024-07-23 10:47:58 -0600

    debug: vercel install scripts

--- scripts/vercel-install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Install the necessary packages
-apt-get update && apt-get install -y libudev-dev libusb-1.0-0-dev
-
-# Continue with the build process
-npm i -g node-gyp
-bun install
