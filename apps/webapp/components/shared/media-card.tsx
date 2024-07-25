75957f53b72312f8334a3e1204b94ce603b97e00
Bran18 <andreyfdez18@gmail.com>
2024-07-25 15:49:17 -0600

    (webapp)fix:media-card text-start

--- apps/webapp/components/shared/media-card.tsx
+++ apps/webapp/components/shared/media-card.tsx
@@ -12,7 +12,7 @@ export function MediaCard({ video, lang }: MediaCardProps) {
       lang={lang}
       video={video}
       trigger={
-        <Card className="p-0 overflow-hidden list-none bg-transparent cursor-pointer group border-muted md:max-h-space-465">
+        <Card className="p-0 overflow-hidden list-none bg-transparent cursor-pointer border-muted group md:max-h-space-465">
           <div className="m-3">
             <figure className="relative h-[215px] w-full overflow-hidden rounded-md">
               <Image
@@ -33,8 +33,8 @@ export function MediaCard({ video, lang }: MediaCardProps) {
             </figure>
           </div>
           <CardContent className="px-3 mt-2">
-            <p className="pt-3 font-semibold leading-tight text-center mb-overflow-hidden truncate_text truncate_text--3-lines text-md">
-              <Balancer>{video.snippet.title}</Balancer>
+          <p className="pt-3 font-semibold leading-tight text-start mb-overflow-hidden truncate_text truncate_text--3-lines text-md">
+          <Balancer>{video.snippet.title}</Balancer>
             </p>
           </CardContent>
         </Card>
