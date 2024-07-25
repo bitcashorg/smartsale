8d386c9ee15319eb2ba5400d4827ddc6e7c84d66
Bran18 <andreyfdez18@gmail.com>
2024-07-25 15:45:44 -0600

    (webapp)fix:Upcoming grid gap

--- apps/webapp/components/routes/home/upcoming.tsx
+++ apps/webapp/components/routes/home/upcoming.tsx
@@ -8,7 +8,7 @@ export function Upcoming({ dict, projects }: UpcomingProps) {
       <div className="relative flex justify-center w-full text-center">
         <div className="absolute top-0 blur-effect-bg" />
       </div>
-      <div className="grid self-center grid-cols-1 gap-6 narrow-container scroll-m-3 auto-rows-fr lg:grid-cols-3 lg:self-stretch">
+      <div className="grid self-center grid-cols-1 gap-[53px] narrow-container scroll-m-3 auto-rows-fr lg:grid-cols-3 lg:self-stretch">
         {projects.map((item, index) => (
           <AuctionCard
             key={`upcoming-auctions-item-${index}`}
