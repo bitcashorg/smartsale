24bece142edf67a81ce5b2d2a4846f8915668aac
Bran18 <andreyfdez18@gmail.com>
2024-07-26 09:18:33 -0600

    (webapp)fix:upcoming bg shape

--- apps/webapp/components/routes/home/upcoming.tsx
+++ apps/webapp/components/routes/home/upcoming.tsx
@@ -5,8 +5,8 @@ import { Project } from '@/lib/projects'
 export function Upcoming({ dict, projects }: UpcomingProps) {
   return (
     <Section heading={dict.home.upcomingHeading}>
-      <div className="absolute flex justify-center w-full text-center">
-        <div className="top-0 blur-effect-bg" />
+      <div className="relative flex justify-center w-full text-center">
+        <div className="absolute top-0 blur-effect-bg" />
       </div>
       <div className="grid self-center grid-cols-1 gap-[53px] narrow-container scroll-m-3 auto-rows-fr lg:grid-cols-3 lg:self-stretch">
         {projects.map((item, index) => (
