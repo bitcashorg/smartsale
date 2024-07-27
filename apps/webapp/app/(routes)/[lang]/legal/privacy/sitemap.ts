1d5e25758a717c72bf5eaf63a222a558a711957e
TopETH <supernathanliu@gmail.com>
2024-07-26 21:09:23 -0400

    fix: add 'async' to all sitemaps

--- apps/webapp/app/(routes)/[lang]/legal/privacy/sitemap.ts
+++ apps/webapp/app/(routes)/[lang]/legal/privacy/sitemap.ts
@@ -1,9 +1,9 @@
 import { CommonPageProps } from '@/types/routing.type'
 import { MetadataRoute } from 'next'
 
-export default function sitemap({
+export default async function sitemap({
   params
-}: CommonPageProps): MetadataRoute.Sitemap {
+}: CommonPageProps): Promise<MetadataRoute.Sitemap> {
   return [
     {
       url: `https://${process.env.VERCEL_URL}/${params.lang}/legal/privacy`,
