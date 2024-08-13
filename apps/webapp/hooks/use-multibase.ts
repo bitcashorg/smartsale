96f868a90a5232a7158c7d2e5762f440c2b02c6f
Roberto Lucas <andre.rlucas@outlook.com>
2024-08-13 04:28:50 -0600

    impr(fix): multibase init call

--- apps/webapp/hooks/use-multibase.ts
+++ apps/webapp/hooks/use-multibase.ts
@@ -1,27 +1,28 @@
-import { init, identify, track } from '@multibase/js'
+import { appConfig } from '@/lib/config'
+import { identify, init, track } from '@multibase/js'
 
-const MULTIBASE_API_KEY = process.env.MULTIBASE_API_KEY || ""
+const multibaseKey = appConfig.multibase.key
 
+if (!multibaseKey) {
+  console.error("Missing MULTIBASE_API_KEY")
+} else {
+  init(multibaseKey)
+  console.info("Multibase Initialized")
+}
 
 export function useMultibase() {
-    if (!MULTIBASE_API_KEY) {
-        throw new Error("Missing MULTIBASE_API_KEY")
-    }
-    
-    init(MULTIBASE_API_KEY)
+  const identifyUser = (address: `0x${string}`, properties: Record<string, any>) =>
+    identify({
+      address,
+      properties
+    })
 
-    const identifyUser = (address: `0x${string}`, properties: Record<string, any>) =>
-        identify({
-            address,
-            properties
-        })
+  const trackInteraction = (label: string, properties: MultibaseTrackPropertiesProps) =>
+    track(label, properties)
 
-    const trackInteraction = (label: string, properties: MultibaseTrackPropertiesProps) =>
-        track(label, properties)
-
-    return { trackInteraction, identifyUser }
+  return { trackInteraction, identifyUser }
 }
 
 interface MultibaseTrackPropertiesProps {
-    [key: string]: any;
+  [key: string]: any;
 }
