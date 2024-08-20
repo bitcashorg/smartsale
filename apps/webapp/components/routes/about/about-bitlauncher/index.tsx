22999878202ae6edb0546df4a69867fb3de6d29a
Bran18 <andreyfdez18@gmail.com>
2024-08-20 00:33:18 -0600

    (wepapp)impr:refactored about us section

--- apps/webapp/components/routes/about/about-bitlauncher/index.tsx
+++ apps/webapp/components/routes/about/about-bitlauncher/index.tsx
@@ -3,6 +3,8 @@ import { CommunityCard } from '@/components/shared/community-card'
 import { getDictionary } from '@/dictionaries'
 import { Lang } from '@/dictionaries/locales'
 import Image from 'next/image'
+import { LearnSection } from '@/components/layout/section/learn-section'
+import StepsSection from '@/components/layout/section/steps-section'
 
 export async function AboutBitlauncherPageLanding({
   content,
@@ -12,45 +14,20 @@ export async function AboutBitlauncherPageLanding({
   const dict = await getDictionary(lang)
   return (
     <>
-      <section className="w-full py-12 narrow-container md:py-24 lg:py-32">
-        <div className="px-0 md:px-6 grid items-center justify-between gap-4 lg:grid-cols-2">
+      <section className="narrow-container pb-[100px] pt-[80]">
+        <div className="grid items-center justify-between gap-4 px-0 md:px-6 lg:grid-cols-2">
           <div className="space-y-4">
-            <h2 className="text-7xl">
+            <h2 className="text-[87px] font-normal leading-normal">
               {content.title}
             </h2>
-            <section className="leading-snug text-[#9395af]">
-              Bitlauncher is a{' '}
-              <b>pioneering launchpad</b>
-              {' '}
-              dedicated to transforming the landscape of artificial
-              intelligence (AI) and cryptocurrency. We are on a mission to
-              empower the next wave of AI innovation by providing
-              open-source AI projects with equitable fundraising
-              opportunities and decentralized organization through the use
-              of blockchain technology.
+            <section className="text-sm font-normal leading-normal opacity-60">
+              {content.paragraph}
               <br />
               <br />
-              At Bitlauncher, we{' '}
-              <b>
-                combine the transformative powers of AI and cryptocurrency
-              </b>
-              {' '}
-              to address the unique challenges faced by AI startups. By
-              integrating tokenization and decentralized autonomous
-              organizations (DAOs), we create a seamless synergy that
-              enables these startups to overcome funding barriers,
-              accelerate their growth, and harness global resources.
+              {content.paragraph2}
               <br />
               <br />
-              Our platform is built on a foundation of{' '}
-              <b className="leading-snug">
-                transparency, inclusivity, and community-driven progress
-              </b>
-              . We foster a collaborative environment where developers,
-              contributors, and AI enthusiasts can come together to share
-              resources, exchange ideas, and shape the future of technology.
-              <br />
-              <br />
+              {content.paragraph3}
             </section>
           </div>
           <div className="infopages-background infopages-background--about">
@@ -59,8 +36,10 @@ export async function AboutBitlauncherPageLanding({
             </div>
           </div>
         </div>
+        <Banner />
+        <StepsSection lang={lang} dict={dict} />
+        <LearnSection />
       </section>
-      <Banner />
     </>
   )
 }
@@ -74,7 +53,9 @@ interface Image {
 
 export interface AboutBitlauncherPageContent {
   title: string
-  description: string
+  paragraph: React.ReactNode
+  paragraph2: React.ReactNode
+  paragraph3: React.ReactNode
   image: Image
 }
 
--- apps/webapp/components/shared/banner.tsx
+++ apps/webapp/components/shared/banner.tsx
@@ -1,35 +1,49 @@
+import { AboutBitlauncherPageContent } from '@/components/routes/about/about-bitlauncher'
 
 export function Banner() {
   return (
-    <section className="w-full py-6 narrow-container">
+    <section className="w-full pt-[80px]">
       <div className="inline-flex flex-col items-center justify-start gap-[37px]">
-        <h2 className="font-['Futura PT'] heading text-center text-4xl font-medium leading-[42.35px] text-neutral-50">
-          Driving Global AI And Crypto Advancement
-          <br />
-          Through Strategic Tokenomics
+        <h2 className="leading-[42.35px text-center text-4xl font-medium heading2">
+          {content.title}
         </h2>
         <div className="h-[2.37px] w-[118.61px] bg-[#ff51ed]"></div>
-        <p className="text-center text-[15px] font-normal paragraph">
-          Through our{' '}
-          <b>
-            unique tokenomics
-          </b>
-          {' '}
-          , including a dynamic burn and mint model, we ensure that our
-          tokens reflect true economic value, promoting stability and growth
-          within our ecosystem. Our global-first approach ensures that we
-          cater to a worldwide audience, providing tools and resources that
-          are accessible to innovators around the globe.
-          <b>
-            {' '}
-            Join us at Bitlauncher
-          </b>
-          {' '}
-          as we pave the way for a new era of technological advancement and
-          community empowerment in the AI and crypto space.
-        </p >
-      </div >
+        <div className='narrow-container'>
+          <p className="paragraph text-center text-[15px] font-normal">
+            {content.paragraph}
+          </p>
+          <br />
+          <p className="paragraph text-center text-[15px] font-normal">
+            {content.callToAction}
+          </p>
+        </div>
+      </div>
+    </section>
+  )
+}
 
-    </section >
+const content = {
+  title: (
+    <>
+      Driving Global AI And Crypto Advancement
+      <br />
+      Through Strategic Tokenomics
+    </>
+  ),
+  paragraph: (
+    <>
+      Through our <strong className='text-white'>unique tokenomics,</strong> including a dynamic burn
+      and mint model, we ensure that our tokens reflect true economic value,
+      promoting stability and growth within our ecosystem. Our global-first
+      approach ensures that we cater to a worldwide audience, providing tools
+      and resources that are accessible to innovators around the globe.
+    </>
+  ),
+  callToAction: (
+    <>
+      <strong className='text-white'>Join us at Bitlauncher</strong> as we pave the way for a new era
+      of technological advancement and community empowerment in the AI and
+      crypto space.
+    </>
   )
 }
