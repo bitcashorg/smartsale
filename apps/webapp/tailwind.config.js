b15895575a52c134a7c1d672aedfdf51d4b8ff43
Roberto Lucas <andler.dev@gmail.com>
2024-08-22 12:19:11 -0600

    fix: tailwind font config

--- apps/webapp/tailwind.config.js
+++ apps/webapp/tailwind.config.js
@@ -94,12 +94,6 @@ module.exports = {
           200: 'rgba(125, 129, 217, 0.43)',
         },
       },
-      background: {
-        cornflowerblue: {
-          100: "rgba(125, 129, 217, 0.5)",
-          200: "rgba(125, 129, 217, 0.43)"
-        },
-      },
       borderRadius: {
         lg: 'var(--radius)',
         md: 'calc(var(--radius) - 2px)',
@@ -114,14 +108,8 @@ module.exports = {
       fontFamily: {
         sans: ['Futura PT', ...fontFamily.sans],
         'futura-pt-bold': ['Futura PT Bold', 'sans-serif'],
-        'futura-pt-condensed': ['Futura PT Heavy', 'sans-serif'],
         'futura-pt-demi': ['Futura PT Demi', 'sans-serif'],
         'lufga-bold': 'var(--font-lufga-bold)',
-      },
-      fontFamily: {
-        sans: ["Futura PT", ...fontFamily.sans],
-        'futura-pt-bold': ['Futura PT Bold', 'sans-serif'],
-        'futura-pt-condensed': ['Futura PT Heavy', 'sans-serif'],
       },
       keyframes: {
         'accordion-down': {
