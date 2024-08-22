34c29d45b3bd0c86b4e15e99e19a0ea9e3baaf2e
Gabo Esquivel <contact@gaboesquivel.com>
2024-08-22 17:46:52 -0600

    debug(indexer): alchemy hook handler

--- apps/indexer/src/routes/index.ts
+++ apps/indexer/src/routes/index.ts
@@ -24,12 +24,12 @@ export function startExpress() {
   // Security Middlewares
   // app.use(helmet())
 
-  app.use(
-    rateLimit({
-      windowMs: 60 * 1000, // 1 minute
-      max: 100, // limit each IP to 100 requests per windowMs
-    }),
-  )
+  // app.use(
+  //   rateLimit({
+  //     windowMs: 60 * 1000, // 1 minute
+  //     max: 100, // limit each IP to 100 requests per windowMs
+  //   }),
+  // )
 
   // Logging middleware
   // app.use(pinoHttp({ 
