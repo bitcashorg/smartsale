e45981ee8faa91d08cd72bd2fe5f75cd2d014928
Gabo Esquivel <contact@gaboesquivel.com>
2024-08-23 01:37:25 -0600

    chore: prod network conig, validate networks

--- apps/indexer/src/routes/alchemy.ts
+++ apps/indexer/src/routes/alchemy.ts
@@ -4,6 +4,25 @@ import { addressActivityTask } from '@repo/trigger'
 import type { Request, Response } from 'express'
 import { appConfig } from '~/config'
 import { logger } from '~/lib/logger'
+import { Network } from 'alchemy-sdk'
+import { prodChains } from 'app-env'
+
+
+const chainIdToNetwork: Record<number, Network> = {
+  1: Network.ETH_MAINNET,
+  137: Network.MATIC_MAINNET,
+  42161: Network.ARB_MAINNET,
+  10: Network.OPT_MAINNET,
+  8453: Network.BASE_MAINNET,
+  43114: Network.AVAX_MAINNET,
+  56: Network.BNB_MAINNET
+}
+
+const networks: Network[] = prodChains.map(chain => {
+  const network = chainIdToNetwork[chain.id]
+  if (!network)  throw new Error(`Unsupported chain ID: ${chain.id}`)
+  return network
+})
 
 /**
  * Handles incoming Alchemy webhook requests.
@@ -14,10 +33,14 @@ import { logger } from '~/lib/logger'
 export async function alchemyWebhook(req: Request, res: Response) {
   const evt = req.body as AlchemyWebhookEvent
   logger.info(`Alchemy webhook received: ${evt.id}`)
+    const auth = evt.type === 'ADDRESS_ACTIVITY' && networks.includes((evt.event).network)
+
   // TODO: restore alchemy signature validation
   //   if (!validateAlchemySignature(req)) return res.status(401).send('Unauthorized')
   //   logger.info('Validated Alchemy webhook 😀')
-  // TODO: validate user is whitelisted
+  // TODO: validate addre is whitelisted
+
+
 
   const handle = await addressActivityTask.trigger(req.body)
   logger.info(`Triggered address activity task: ${JSON.stringify(handle)}`)
