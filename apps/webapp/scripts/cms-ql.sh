4febf8551681af2f4670f64a231583cbf8226c73
Roberto Lucas <andler.dev@gmail.com>
2024-08-23 19:40:19 -0600

    docs: upt cms-ql.sh
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>

--- apps/webapp/scripts/cms-ql.sh
+++ apps/webapp/scripts/cms-ql.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+
+# cms-ql.sh: Utility script for generating GraphQL types
+# Usage: ./cms-ql.sh
+# Dependencies: GraphQL schema must be properly configured
 # eval $(grep '^NEXT_PUBLIC_CMS_GRAPHQL_API' ./.env) && genql --endpoint $NEXT_PUBLIC_CMS_GRAPHQL_API --output ./graphql/generated/cms -H 'content-type: application/json'
 # eval $(grep '^NEXT_PUBLIC_CMS_GRAPHQL_API' ./.env) && eval $(grep '^NEXT_PUBLIC_CMS_API_KEY' ./.env) && genql --endpoint $NEXT_PUBLIC_GRAPHQL_API --output ./graphql/generated/cms -H 'content-type: application/json' -H 'Authorization: Bearer $NEXT_PUBLIC_CMS_API_KEY'
 eval $(grep '^NEXT_PUBLIC_CMS_GRAPHQL_API' ./.env) && \
