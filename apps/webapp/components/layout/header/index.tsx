fcdc368ccb0a1e9364e16e711d5e6ea5372bb43b
Bran18 <andreyfdez18@gmail.com>
2024-08-26 13:22:52 -0600

    webapp:fix margin top

--- apps/webapp/components/layout/header/index.tsx
+++ apps/webapp/components/layout/header/index.tsx
@@ -14,8 +14,8 @@ import { Navigation } from './new-nav'
 
 export function Header({ lang, dict }: HeaderProps) {
   return (
-    <div className="sticky top-0 z-50 bg-background md:mt-8">
-      <div className="container flex items-center justify-between h-16 mx-auto md:px-4">
+    <div className="sticky top-0 z-50 bg-background md:mt-4">
+      <div className="container flex items-center justify-between h-16 mx-auto md:px-4 md:-top-4">
         {/* Left Section with Icon */}
         <div className="flex items-center justify-start flex-1">
           <Link href={`/${lang}`}>
