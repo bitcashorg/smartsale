2708e0634878971851c894b680323872281d7e14
Bran18 <andreyfdez18@gmail.com>
2024-09-05 11:37:28 -0600

    impr:coderabit section impr + andlers

--- apps/webapp/components/shared/section.tsx
+++ apps/webapp/components/shared/section.tsx
@@ -5,7 +5,7 @@ export function Section({ heading, children, id }: SectionProps) {
   return (
     <section
       className="flex flex-col justify-center pt-20 text-center align-center lg:pt-40"
-      id={id}
+      {...(id ? { id } : {})}
     >
       <h2 className="relative z-10 flex justify-center w-full pb-10 text-center shadow-sm heading">
         <Balancer>{heading}</Balancer>
