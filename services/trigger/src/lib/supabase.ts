5c9c0560233181c8cc7a701e9d62ac24e957b660
Gabo Esquivel <contact@gaboesquivel.com>
2024-09-24 09:19:35 -0600

    hotfix(web): use project id 1

--- services/trigger/src/lib/supabase.ts
+++ services/trigger/src/lib/supabase.ts
@@ -218,6 +218,7 @@ export async function getWhitelistedAddress(account: string) {
     .from('whitelist')
     .select('*')
     .eq('account', account)
+    .eq('project_id', 1) // TODO: make this dynamic
     .single()
 
   if (error || !data.address) {
