c3723072d985ae7a7e805cbf4e037cc42f08ac88
Gabo Esquivel <contact@gaboesquivel.com>
2024-09-24 06:47:38 -0600

    chore: update eos esr callback handler

--- apps/web/app/api/esr/route.ts
+++ apps/web/app/api/esr/route.ts
@@ -1,217 +1,225 @@
-import { deflateRawSync, inflateRawSync } from 'zlib'
-import { savePresaleDepositIntent } from '@/app/actions/save-deposit'
-import { appConfig } from '@/lib/config'
-import { createSupabaseServerClient } from '@/services/supabase'
-import { insertTransaction } from '@/services/supabase/service'
-import { getErrorMessage } from '@repo/errors'
-import { tasks } from '@trigger.dev/sdk/v3'
-import { ABI, APIClient, Action } from '@wharfkit/antelope'
+import { deflateRawSync, inflateRawSync } from "zlib";
+import { savePresaleDepositIntent } from "@/app/actions/save-deposit";
+import { appConfig } from "@/lib/config";
+import { createSupabaseServerClient } from "@/services/supabase";
+import { insertTransaction } from "@/services/supabase/service";
+import { getErrorMessage } from "@repo/errors";
+import { tasks } from "@trigger.dev/sdk/v3";
+import { ABI, APIClient, Action } from "@wharfkit/antelope";
 import {
-  type AbiProvider,
-  SigningRequest,
-  type SigningRequestEncodingOptions,
-  type ZlibProvider,
-} from 'eosio-signing-request'
-import { type NextRequest, NextResponse } from 'next/server'
-import { z } from 'zod'
+	type AbiProvider,
+	SigningRequest,
+	type SigningRequestEncodingOptions,
+	type ZlibProvider,
+} from "eosio-signing-request";
+import { type NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
 
 export async function POST(req: NextRequest) {
-  try {
-    const request = await req.json()
-    console.log('Request', request)
+	try {
+		const request = await req.json();
+		console.log("Request", request);
 
-    const parsed = SigningRequestCallbackPayloadSchema.safeParse(request)
-    if (parsed.error)
-      console.error('Invalid ESR CallbackPayload', parsed.error, parsed.data)
-    if (!parsed.data || parsed.error) throw new Error('Invalid ESR CallbackPayload')
+		const parsed = SigningRequestCallbackPayloadSchema.safeParse(request);
+		if (parsed.error)
+			console.error("Invalid ESR CallbackPayload", parsed.error, parsed.data);
+		if (!parsed.data || parsed.error)
+			throw new Error("Invalid ESR CallbackPayload");
 
-    const payload = parsed.data
-    // callbackPayload example
-    // {
-    //   sp: 'active',
-    //   req: 'esr:gmNgZJhQOG-WUgqfDQMQNLyU6GVkZIAAJijNARNgVsooKSkottLXT0kt00vKLMlJLM1Lzkgt0kvM1E8syNRPLS5iYiktzUxRMUpLSklMMk3SNU0zTNY1MTRM1bVIszTSTU1KMzYzSEtLNrEwYU8sKPBLzE3ldkKYBAA',
-    //   sa: 'gaboesquivel',
-    //   rid: '1398298171',
-    //   bn: '0',
-    //   tx: '45b6258c212c57e7fec4aa726d4153e23987501f308fbca4beea6fbd1052dd0c',
-    //   sig: 'SIG_K1_KdvaUXNswAn8UGLJAyMU7FbATjafeDSSWLgqJpiJ6mg8xUUtroBzGBxxznYS1CqkzhB2X5oX83Zqhp3MAP4oKWV12W5Hdx',
-    //   rbn: '59360',
-    //   ex: '2024-04-18T16:21:19',
-    //   cid: 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906'
-    // }
+		const payload = parsed.data;
+		// callbackPayload example
+		// {
+		//   sp: 'active',
+		//   req: 'esr:gmNgZJhQOG-WUgqfDQMQNLyU6GVkZIAAJijNARNgVsooKSkottLXT0kt00vKLMlJLM1Lzkgt0kvM1E8syNRPLS5iYiktzUxRMUpLSklMMk3SNU0zTNY1MTRM1bVIszTSTU1KMzYzSEtLNrEwYU8sKPBLzE3ldkKYBAA',
+		//   sa: 'gaboesquivel',
+		//   rid: '1398298171',
+		//   bn: '0',
+		//   tx: '45b6258c212c57e7fec4aa726d4153e23987501f308fbca4beea6fbd1052dd0c',
+		//   sig: 'SIG_K1_KdvaUXNswAn8UGLJAyMU7FbATjafeDSSWLgqJpiJ6mg8xUUtroBzGBxxznYS1CqkzhB2X5oX83Zqhp3MAP4oKWV12W5Hdx',
+		//   rbn: '59360',
+		//   ex: '2024-04-18T16:21:19',
+		//   cid: 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906'
+		// }
 
-    const esr = SigningRequest.from(payload.req, esrNodeJSOptions)
+		const esr = SigningRequest.from(payload.req, esrNodeJSOptions);
 
-    console.log('ESR callback payload', payload, esr.toJSON())
-    const id = esr.getInfoKey('uuid')
-    const action = esr.data.req.toJSON()
-    const isPresale = Boolean(esr.getInfoKey('presale'))
+		console.log("ESR callback payload", payload, esr.toJSON());
+		const id = esr.getInfoKey("uuid");
 
-    console.log('👋 esr data input ', JSON.stringify({ id, action, isPresale, esr }))
+		const actionName = esr.getRawActions()[0].name.toString();
+		const isLogin = actionName === "login";
+		const isPresale = Boolean(esr.getInfoKey("presale"));
+		const abis = await esr.fetchAbis();
+		const action = esr.resolveActions(abis)[0];
 
-    // esr request with trx id
-    // TODO: review this logic, inserting for now on single go
-    const supabase = await createSupabaseServerClient()
-    const { data: esrUpdate, error } = await supabase
-      .from('esr')
-      .insert({
-        id,
-        code: payload.req,
-        account: payload.sa,
-        trx_id: payload.tx,
-        created_at: new Date().toISOString(),
-      })
-      .select('*')
+		console.log(
+			"👋 esr data input ",
+			JSON.stringify({ id, action, isPresale, esr, abis }),
+		);
 
-    if (error) {
-      throw new Error(`Error updating ESR entry: ${error.message}`)
-    }
-    console.log('ESR entry updated successfully:', esrUpdate)
+		// esr request with trx id
+		// TODO: review this logic, inserting for now on single go
+		const supabase = await createSupabaseServerClient();
+		const { data: esrUpdate, error } = await supabase
+			.from("esr")
+			.insert({
+				id,
+				code: payload.req,
+				account: payload.sa,
+				trx_id: payload.tx,
+				created_at: new Date().toISOString(),
+			})
+			.select("*");
 
-    // create a session is if the signed action is login
-    if (esr.getRawActions()[0].name.toString() === 'login') {
-      const { data: session, error: sessionError } = await supabase
-        .from('session')
-        .insert([
-          {
-            id,
-            tx: payload.tx,
-            account: payload.sa,
-            esr_code: payload.req,
-          },
-        ])
-        .select('*')
+		if (error) {
+			throw new Error(`Error updating ESR entry: ${error.message}`);
+		}
+		console.log("ESR entry updated successfully:", esrUpdate);
 
-      if (sessionError) {
-        throw new Error(`Error creating session: ${sessionError.message}`)
-      }
-      console.log('Session created successfully:', session)
-    }
+		// create a session is if the signed action is login
+		if (isLogin) {
+			const { data: session, error: sessionError } = await supabase
+				.from("session")
+				.insert([
+					{
+						id,
+						tx: payload.tx,
+						account: payload.sa,
+						esr_code: payload.req,
+					},
+				])
+				.select("*");
 
-    if (isPresale) {
-      const transaction = await insertTransaction(
-        {
-          hash: payload.tx,
-          trx_type: 'presale_deposit',
-          final: false,
-          chain_id: 1,
-          chain_type: 'eos',
-        },
-        supabase,
-      )
-      if (!transaction) throw new Error('Error creating transaction')
+			if (sessionError) {
+				throw new Error(`Error creating session: ${sessionError.message}`);
+			}
+			console.log("Session created successfully:", session);
+		}
 
-      // NOTE: hotfix for now, we dont wait finality as indexer is in progress
+		if (isPresale) {
+			const transaction = await insertTransaction(
+				{
+					hash: payload.tx,
+					trx_type: "presale_deposit",
+					final: false,
+					chain_id: 1,
+					chain_type: "eos",
+				},
+				supabase,
+			);
+			if (!transaction) throw new Error("Error creating transaction");
 
-      // const actionData = Action.from(action.data,abi}
+			// NOTE: hotfix for now, we dont wait finality as indexer is in progress
 
-      // const eosDeposit = {
-      //   trxId: payload.tx,
-      //   from: payload.sa,
-      //   quantity: payload.bn,
-      //   to:
-      // }
-      // const result = await tasks.trigger('eos-presale-deposit', eosDeposit)
-      // console.info(
-      //   `Triggered address activity event for webhook ${eosDeposit.trxId}`,
-      //   result,
-      // )
+			// const actionData = Action.from(action.data,abi}
 
-      // TODO: save deposit intent here instead of trigger job
-      // console.log('Transaction hash:', trxHash)
-      // const deposit = await savePresaleDepositIntent({
-      //   amount: Number(parseUnits(amount, 6)),
-      //   created_at: new Date().toISOString(),
-      //   deposit_hash: trxHash,
-      //   issuance_hash: null,
-      //   presale_id: project.presaleId as number,
-      //   address,
-      //   project_id: project.id,
-      //   account: session?.account,
-      //   chain_type: evmToken.chainType,
-      //   chainId: evmToken.chainId,
-      // })
-      // console.log('deposit', deposit)
+			// const eosDeposit = {
+			//   trxId: payload.tx,
+			//   from: payload.sa,
+			//   quantity: payload.bn,
+			//   to:
+			// }
+			// const result = await tasks.trigger('eos-presale-deposit', eosDeposit)
+			// console.info(
+			//   `Triggered address activity event for webhook ${eosDeposit.trxId}`,
+			//   result,
+			// )
 
-      console.log('Saved transaction:', transaction)
-    }
+			// TODO: save deposit intent here instead of trigger job
+			// console.log('Transaction hash:', trxHash)
+			// const deposit = await savePresaleDepositIntent({
+			//   amount: Number(parseUnits(amount, 6)),
+			//   created_at: new Date().toISOString(),
+			//   deposit_hash: trxHash,
+			//   issuance_hash: null,
+			//   presale_id: project.presaleId as number,
+			//   address,
+			//   project_id: project.id,
+			//   account: session?.account,
+			//   chain_type: evmToken.chainType,
+			//   chainId: evmToken.chainId,
+			// })
+			// console.log('deposit', deposit)
 
-    return NextResponse.json({
-      success: true,
-      message: 'Successfully received esr callback',
-    })
-  } catch (error) {
-    console.error('Error handling request:', error)
-    return NextResponse.json({
-      success: false,
-      message:
-        getErrorMessage(error) || 'An error occurred during the request processing',
-    })
-  }
+			console.log("Saved transaction:", transaction);
+		}
+
+		return NextResponse.json({
+			success: true,
+			message: "Successfully received esr callback",
+		});
+	} catch (error) {
+		console.error("Error handling request:", error);
+		return NextResponse.json({
+			success: false,
+			message:
+				getErrorMessage(error) ||
+				"An error occurred during the request processing",
+		});
+	}
 }
 
 const eos = new APIClient({
-  url: appConfig.eosRpc,
-})
-
+	url: appConfig.eosRpc,
+});
 const esrNodeJSOptions: SigningRequestEncodingOptions = {
-  abiProvider: {
-    getAbi: async (account) => {
-      const response = await eos.v1.chain.get_abi(account.toString())
-      return response.abi
-    },
-  } as AbiProvider,
-  zlib: {
-    deflateRaw: (data) => new Uint8Array(deflateRawSync(Buffer.from(data))),
-    inflateRaw: (data) => new Uint8Array(inflateRawSync(Buffer.from(data))),
-  } as ZlibProvider,
-}
+	abiProvider: {
+		getAbi: async (account) => {
+			const response = await eos.v1.chain.get_abi(account.toString());
+			return response.abi;
+		},
+	} as AbiProvider,
+	zlib: {
+		deflateRaw: (data) => new Uint8Array(deflateRawSync(Buffer.from(data))),
+		inflateRaw: (data) => new Uint8Array(inflateRawSync(Buffer.from(data))),
+	} as ZlibProvider,
+};
 
 const SigningRequestCallbackPayloadSchema = z.object({
-  sp: z.string(),
-  req: z.string(),
-  sa: z.string(),
-  rid: z.string(),
-  bn: z.string().optional(),
-  tx: z.string(),
-  sig: z.string(),
-  rbn: z.string(),
-  ex: z.string().optional(),
-  // ex: z.string().refine(arg => !isNaN(Date.parse(arg)), {
-  //   message: 'ex must be a valid ISO date string'
-  // }),
-  cid: z.string().optional(),
-})
+	sp: z.string(),
+	req: z.string(),
+	sa: z.string(),
+	rid: z.string(),
+	bn: z.string().optional(),
+	tx: z.string(),
+	sig: z.string(),
+	rbn: z.string(),
+	ex: z.string().optional(),
+	// ex: z.string().refine(arg => !isNaN(Date.parse(arg)), {
+	//   message: 'ex must be a valid ISO date string'
+	// }),
+	cid: z.string().optional(),
+});
 
 const abi = ABI.from({
-  version: 'eosio::abi/1.0',
-  types: [],
-  variants: [],
-  structs: [
-    {
-      name: 'transfer',
-      base: '',
-      fields: [
-        {
-          name: 'from',
-          type: 'name',
-        },
-        {
-          name: 'to',
-          type: 'name',
-        },
-        {
-          name: 'quantity',
-          type: 'asset',
-        },
-        {
-          name: 'memo',
-          type: 'string',
-        },
-      ],
-    },
-  ],
-  actions: [],
-  tables: [],
-  ricardian_clauses: [],
-})
+	version: "eosio::abi/1.0",
+	types: [],
+	variants: [],
+	structs: [
+		{
+			name: "transfer",
+			base: "",
+			fields: [
+				{
+					name: "from",
+					type: "name",
+				},
+				{
+					name: "to",
+					type: "name",
+				},
+				{
+					name: "quantity",
+					type: "asset",
+				},
+				{
+					name: "memo",
+					type: "string",
+				},
+			],
+		},
+	],
+	actions: [],
+	tables: [],
+	ricardian_clauses: [],
+});
