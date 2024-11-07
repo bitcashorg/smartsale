13d5b843414a34bb42943fe16b0632e1e56bebdf
Leandro Gavidia Santamaria <leandro.d3v@gmail.com>
2024-11-07 12:45:39 -0400

    feature: add validation to create short link

--- apps/web/scripts/generate-cms-dictionaries.ts
+++ apps/web/scripts/generate-cms-dictionaries.ts
@@ -2,11 +2,14 @@ import {
   getArticleSections,
   getBlogCategoryLandingData,
   getBlogArticleData,
+  BlogArticleRecord,
 } from "@/services/datocms";
 
 import { AVAILABLE_LANGS } from "@/lib/config";
 import { Lang } from "@/dictionaries/locales";
 import { generateShortLink } from "@/app/actions/general";
+import path from "node:path";
+import * as fs from "fs";
 
 async function getArticleCategories(lang: Lang) {
   const categoriesData = await getArticleSections(lang as Lang);
@@ -16,9 +19,25 @@ async function getArticleCategories(lang: Lang) {
 async function addShortLinksForArticles(lang: Lang, category: string, slugs: string[]) {
   const shortLinkPromises = slugs.map(async (slug) => {
     await getBlogArticleData(lang as Lang, category, slug);
-    console.log(`${lang}/${category}/${slug}`)
-    const shortLink = await generateShortLink(`https://bitlauncher.ai/${lang}/${category}/${slug}`, false);
-    return { category, lang, slug, shortLink: shortLink.data?.shortLink || "" };
+    const canonicalUrl = `https://bitlauncher.ai/${lang}/${category}/${slug}`;
+    const shortLink = (await generateShortLink(canonicalUrl, false)).data?.shortLink;
+
+    const dirPath = `./dictionaries/${lang}/blog/${category}`;
+    const fileName = `${slug}.json`;
+    const filePath = path.resolve(dirPath, fileName);
+    try {
+      const data = await fs.promises.readFile(filePath, "utf-8");
+      const json = JSON.parse(data);
+      if (!json.shortLink) {        
+        json["shortLink"] = shortLink || "";
+        const updatedJson = JSON.stringify(json, null, 2);
+        await fs.promises.writeFile(filePath, updatedJson, "utf-8");
+      }
+    } catch (e) {
+      console.log("Error adding the short link in:", filePath)
+    }
+
+    return { category, lang, slug, shortLink: shortLink || "" };
   });
   await Promise.all(shortLinkPromises);
 }
@@ -34,6 +53,34 @@ async function main() {
       await addShortLinksForArticles(lang as Lang, category, slugs);
     }
     langIndex++;
+  }
+}
+
+async function addShortLink({
+  category,
+  lang,
+  slug,
+}: {
+  lang: Lang;
+  category: string;
+  slug: string;
+}) {
+  try {
+    const dirPath = `./dictionaries/${lang}/blog/${category}`;
+    const fileName = `${slug}.json`;
+    const filePath = path.resolve(dirPath, fileName);
+    const canonicalUrl = `https://bitlauncher.ai/${lang}/${category}/${slug}`;
+    const shortLink = (await generateShortLink(canonicalUrl, false)).data
+      ?.shortLink;
+
+    const data = await fs.promises.readFile(filePath, "utf-8");
+    const json = JSON.parse(data);
+    json["shortLink"] = shortLink || "";
+    const updatedJson = JSON.stringify(json, null, 2);
+    await fs.promises.writeFile(filePath, updatedJson, "utf-8");
+    console.log(`Field "shortLink" added successfully.`);
+  } catch (e) {
+    console.error("Error adding short link:", e);
   }
 }
 
